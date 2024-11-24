ff3dfe581db6d4672fdc52340d45d69d0df1076e
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-11-24 12:07:36 +0100

    Fix #14

--- data.lua
+++ data.lua
@@ -2,6 +2,9 @@ _G.SP_BLACKLIST = {
   ['wall-solar-panel'] = true,      -- Solar Walls
   ['accumulator-interface'] = true, -- Accumulator Walls
   ['compaktcircuit-accu'] = true,   -- Compact Circuits
+  ['personal-transformer-output-entity'] = true,     -- PersonalTransformer2
+  ['personal-transformer-mk2-output-entity'] = true, -- PersonalTransformer2
+  ['personal-transformer-mk3-output-entity'] = true, -- PersonalTransformer2
 }
 
 require '__solar-productivity__.prototypes.technology'

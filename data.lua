e8d30e8bfa4fdfb18d5909ff2d8b97b9eed70139
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-11-24 10:05:22 +0100

    release v2.1.1

--- data.lua
+++ data.lua
@@ -1,6 +1,7 @@
 _G.SP_BLACKLIST = {
-  ['wall-solar-panel'] = true,
-  ['accumulator-interface'] = true,
+  ['wall-solar-panel'] = true,      -- Solar Walls
+  ['accumulator-interface'] = true, -- Accumulator Walls
+  ['compaktcircuit-accu'] = true,   -- Compact Circuits
 }
 
 require '__solar-productivity__.prototypes.technology'

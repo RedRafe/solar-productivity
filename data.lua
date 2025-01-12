bdd7b6be40af14da478617bccf7f4f8be709ab9c
danielmartin0 <github@danielgmartin.com>
2025-01-12 20:05:42 +0000

    Compatibility with Cerys (#15)
    * Cerys compat
    
    * Update info.json

--- data.lua
+++ data.lua
@@ -6,6 +6,7 @@ _G.SP_BLACKLIST = {
   ['personal-transformer-mk2-output-entity'] = true, -- PersonalTransformer2
   ['personal-transformer-mk3-output-entity'] = true, -- PersonalTransformer2
   ['james-rail-accumulator'] = true, -- James' Electric Trains Plus
+  ['cerys-charging-rod'] = true, -- Cerys' Charging Rod
 }
 
 require '__solar-productivity__.prototypes.technology'

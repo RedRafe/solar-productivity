a999ac721b03294df4f4496b5f6a5cc61c232d68
ldinc <drogunov.igor@gmail.com>
2025-09-26 10:01:52 +0300

    v2.1.12 compat railgun artillery mod

--- data.lua
+++ data.lua
@@ -1,22 +1,23 @@
 _G.SP_BLACKLIST = {
-  ['wall-solar-panel'] = true,      -- Solar Walls
-  ['accumulator-interface'] = true, -- Accumulator Walls
-  ['compaktcircuit-accu'] = true,   -- Compact Circuits
+  ['wall-solar-panel'] = true,                       -- Solar Walls
+  ['accumulator-interface'] = true,                  -- Accumulator Walls
+  ['compaktcircuit-accu'] = true,                    -- Compact Circuits
   ['personal-transformer-output-entity'] = true,     -- PersonalTransformer2
   ['personal-transformer-mk2-output-entity'] = true, -- PersonalTransformer2
   ['personal-transformer-mk3-output-entity'] = true, -- PersonalTransformer2
-  ['james-rail-accumulator'] = true, -- James' Electric Trains Plus
-  ['cerys-charging-rod'] = true, -- Cerys' Charging Rod
-  ['kr-intergalactic-transceiver'] = true, -- Krastorio2
-  ['warp-power'] = true, -- warptorio-space-age
-  ['warp-power-2'] = true, -- warptorio-space-age
-  ['warp-power-3'] = true, -- warptorio-space-age
-  ['se-spaceship-console'] = true, -- Space Exploration
-  ['muluna-satellite-radar'] = true, -- Muluna
-  ['bi-bio-farm-hidden-panel'] = true, -- bio industries 2
-  ['bi-solar-boiler-hidden-panel'] = true, -- bio industries 2
-  ['bi-musk-mat-hidden-panel'] = true, -- bio industries 2
-  ['se-spaceship-console-alt'] = true, -- Space Exploration
+  ['james-rail-accumulator'] = true,                 -- James' Electric Trains Plus
+  ['cerys-charging-rod'] = true,                     -- Cerys' Charging Rod
+  ['kr-intergalactic-transceiver'] = true,           -- Krastorio2
+  ['warp-power'] = true,                             -- warptorio-space-age
+  ['warp-power-2'] = true,                           -- warptorio-space-age
+  ['warp-power-3'] = true,                           -- warptorio-space-age
+  ['se-spaceship-console'] = true,                   -- Space Exploration
+  ['muluna-satellite-radar'] = true,                 -- Muluna
+  ['bi-bio-farm-hidden-panel'] = true,               -- bio industries 2
+  ['bi-solar-boiler-hidden-panel'] = true,           -- bio industries 2
+  ['bi-musk-mat-hidden-panel'] = true,               -- bio industries 2
+  ['se-spaceship-console-alt'] = true,               -- Space Exploration,
+  ['ldinc-railgun-artillery-power-unit'] = true,     -- Railgun Artillery
 }
 
 require '__solar-productivity__.prototypes.technology'
